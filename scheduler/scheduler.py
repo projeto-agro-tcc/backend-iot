26d29bc489f3bf4abd8cbb7cfbd877a205d7062a
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2022-02-11 08:45:10 -0300

    static files

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -3,7 +3,7 @@ from scheduler.tasks.task_catch_data import CatchDataFromReceivedData
 
 
 def start():
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler(timezone='Brazil/East')
     catch_data = CatchDataFromReceivedData()
     scheduler.add_job(catch_data.get_data, "interval", minutes=1, id="task_001", replace_existing=True)
     scheduler.start()
