b31f5830eeb03e145112da1baf626a4765f39d20
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2022-02-11 11:30:39 -0300

    teste deploy

--- backend_iot/settings_prod.py
+++ backend_iot/settings_prod.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'backend_iot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-URI_MONGO = "mongodb://root:ort4344svfa0098ii9o@127.0.0.1:27017"
+URI_MONGO = "mongodb://root:ort4344svfa0098ii9o@localhost:27017"
 DB_NAME = 'iot_monitoramento'
 RECEIVED_DATADB = 'received_data'
 
