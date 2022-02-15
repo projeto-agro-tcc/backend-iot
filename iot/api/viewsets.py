57503a4731c0fbf10a2668f6124505e0bb38bb9a
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2022-02-15 13:59:48 -0300

    ajuste resposta api

--- iot/api/viewsets.py
+++ iot/api/viewsets.py
@@ -1,14 +1,16 @@
+import json
 from datetime import datetime
-
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from iot.api.IotService import *
+from iot.api.serializer import EmwSerializer
 
 
 class IotViewSet(ModelViewSet):
+    serializer_class = EmwSerializer
 
     @action(detail=False, methods=['GET'])
     def findbyparams(self, request, *args, **kwargs):
@@ -20,6 +22,7 @@ class IotViewSet(ModelViewSet):
             if time_to_end is None:
                 time_to_end = datetime.now().timestamp()
             response = IotService.getDataByParams(time_to_start, time_to_end, dev_id, colection)
-            return Response(response, status=status.HTTP_200_OK)
+            result_data = EmwSerializer(json.loads(response), many=True).data
+            return Response(result_data, status=status.HTTP_200_OK)
         else:
             return Response({"detail": "Verify the fields"}, status=status.HTTP_400_BAD_REQUEST)
