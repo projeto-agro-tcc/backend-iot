9ed871ed2886c51f01294114186297314c3addb9
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2022-02-16 22:20:45 -0300

    commit 21:46 16-02

--- backend_iot/enviroments.py
+++ backend_iot/enviroments.py
@@ -1,3 +1,5 @@
+from backend_iot.settings import BASE_DIR
+
 URI_MONGO = "mongodb://admin:123456@127.0.0.1:27017"
 #URI_MONGO = 'mongodb://root:ort4344svfa0098ii9o@localhost:27017'
 DB_NAME = 'iot_monitoramento'
@@ -15,3 +17,19 @@ COLLECTIONS_EMW = {
     "lum" : "emw_luminosity_lux",
     "uv" : "emw_uv_dimensionless"
 }
+
+#database aws
+database = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'backend_iot',
+    'USER': 'root',
+    'PASSWORD': '2021monitoramento',
+    'HOST': 'localhost',
+    'PORT': '',
+}
+
+#database localhost
+# database = {
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
+# }
--- backend_iot/settings.py
+++ backend_iot/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from backend_iot import enviroments
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -64,18 +66,7 @@ WSGI_APPLICATION = 'backend_iot.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'backend_iot',
-    #     'USER': 'root',
-    #     'PASSWORD': '2021monitoramento',
-    #     'HOST': 'localhost',
-    #     'PORT': '',
-    # }
+    'default': enviroments.database
 }
 
 
