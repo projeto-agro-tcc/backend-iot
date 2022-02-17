d3086a016eb1c73130c1f3f6fa06d00d2a19c096
osvaldosneto@hotmail.com <osvaldosneto@hotmail.com>
2022-02-16 22:45:42 -0300

    commit 21:46 16-02

--- backend_iot/enviroments.py
+++ backend_iot/enviroments.py
@@ -19,20 +19,18 @@ COLLECTIONS_EMW = {
 }
 
 #database aws
-database = {'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'backend_iot',
-        'USER': 'root',
-        'PASSWORD': '2021monitoramento',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+database = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'backend_iot',
+    'USER': 'root',
+    'PASSWORD': '2021monitoramento',
+    'HOST': 'localhost',
+    'PORT': '',
 }
+
 
 #database
 # database = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
 # }
